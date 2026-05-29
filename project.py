985968d224b0ec9c4cf7a394f34efe90dab801dc
jchandd <jaydenc243@deltalearns.ca>
2026-05-29 14:30:45 -0700

    added some pseudocode and commenting

---
+++ project.py
@@ -0,0 +1,7 @@
+# App that uses at least three API's and has a GUI user interface.
+# Must return user at least 5 different data points from the API's.
+# GUI interface to ask the user for specific information and then request that information from the API sites
+
+import requests
+import tkinter as tk
+from tkinter import messagebox
