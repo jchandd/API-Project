b6a20f6692f097ad869a9422509a31a6c02cebc1
jchandd <jaydenc243@deltalearns.ca>
2026-06-01 17:13:39 -0700

    added the country info part of my project

--- project.py
+++ project.py
@@ -6,4 +6,29 @@ import requests
 import tkinter as tk
 from tkinter import messagebox
 
-def get_weather(city):  
+GEODB_KEY = ""
+WEATHER_KEY = ""
+EXCHANGE_KEY = ""
+
+
+def get_country_info(country):
+
+    url = f"https://wft-geo-db.p.rapidapi.com/v1/geo/countries?namePrefix={country}"
+
+    headers = {
+        "X-RapidAPI-Key": GEODB_KEY,
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()["data"][0]
+
+    return {
+        "country": data["name"],
+        "code": data["code"],
+        "capital": data.get("capital", "N/A")
+    }
