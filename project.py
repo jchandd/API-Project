a5cc39efc9108978e086aa2ed735af516afee3a5
jchandd <jaydenc243@deltalearns.ca>
2026-06-03 10:22:20 -0700

    added more pseudocode and commenting

--- project.py
+++ project.py
@@ -4,7 +4,7 @@
 
 import requests
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox 
 
 GEODB_KEY = "1b97151aeamshcf5fbd1af73bedbp1535c6jsn90121c4a713a"
 WEATHER_KEY = "cc96d68c73cf139304a381905f85826e"
@@ -20,9 +20,9 @@ def get_country_info(country):
         "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)  # Make the API request with the headers for authentication
 
-    if response.status_code != 200:
+    if response.status_code != 200: 
         return None
 
     data = response.json()["data"][0]
@@ -38,7 +38,7 @@ def get_weather(city):
 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_KEY}&units=metric"
 
-    response = requests.get(url)
+    response = requests.get(url) 
 
     if response.status_code != 200:
         return None
@@ -48,7 +48,7 @@ def get_weather(city):
     return {
         "temp": data["main"]["temp"],
         "humidity": data["main"]["humidity"],
-        "desc": data["weather"][0]["description"]
+        "desc": data["weather"][0]["description"] # Get the weather description from the API response
     }
 
 def get_exchange():
